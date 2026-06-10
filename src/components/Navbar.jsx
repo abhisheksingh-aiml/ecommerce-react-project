1ddd3246e34490fd45683256104cee6cab0d7a92
Abhishek Kumar Singh <abhishekkrsingh77620@gmail.com>
2026-06-10 19:34:54 +0530

    home bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return(
+        <nav>
+            <h1>Cartify</h1>
+
+            <ul>
+                <li>Home</li>
+                <li>Product</li>
+                <li>Cart</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return(
+        <div>
+            <h2>Welcome to My E-Commerce Web</h2>
+            <p>Explore amazing products here</p>
+        </div>
+    );
+}
+
+export default Home;
